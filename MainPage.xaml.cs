de1ebc4bfc84f8799b5f0a747d6498621a799e71
Con McGourty <conmcgourty@yahoo.ie>
2023-11-14 15:57:56 -0800

    Stable binding data with addWatch to service.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -6,62 +6,57 @@ using Map = Microsoft.Maui.Controls.Maps.Map;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Controls.Maps;
 using BeanTea.Services.BeanTeaServices;
+using BeanTea.ViewModels;
+using System.Diagnostics.CodeAnalysis;
 
 namespace BeanTea
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
         private Location selectedLocation;
+        private double distance;
         private readonly Auth0Client auth0Client;
         private readonly WatchService _watchservice;
+        AddWatchEntity _watchEntity;
 
         public MainPage(Auth0Client client, WatchService watchService)
         {
             InitializeComponent();
             auth0Client = client;
-       
+            _watchservice = watchService;
+            _watchEntity = new AddWatchEntity();
+            BindingContext = _watchEntity;
+
+            distance = 2500;
+            selectedLocation = new Location(49.2901, -123.1376);
+            DrawACircleOnTheMap(distance, selectedLocation);
         }
 
         private void OnMapTapped(object sender, MapClickedEventArgs e)
         {
             selectedLocation = e.Location;
+            DrawACircleOnTheMap(distance, e.Location);          
+        }
 
-            var radius = new Circle
+        private void DrawACircleOnTheMap(double radius, Location location)
+        {
+            var circle = new Circle
             {
-                Center = selectedLocation,
-                Radius = Distance.FromMeters(SearchSlider.Value),
+                Center = location,
+                Radius = Distance.FromMeters(radius),
                 StrokeColor = Color.FromHex("#88FF0000"),
                 FillColor = Color.FromHex("#88FFC0CB"),
                 StrokeWidth = 2
             };
 
             map.MapElements.Clear();
-            map.MapElements.Add(radius);
+            map.MapElements.Add(circle);
         }
 
         private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
         {
-            var centerLocation = new Location(49.2901, -123.1376);
-            if (selectedLocation != null)
-            {
-                centerLocation = selectedLocation;
-            }
-
-            var radius = new Circle
-            {
-                Center = centerLocation,
-                Radius = Distance.FromMeters(e.NewValue),
-                StrokeColor = Color.FromHex("#88FF0000"),
-                FillColor = Color.FromHex("#88FFC0CB"),
-                StrokeWidth = 2
-            };
-
-            map.MapElements.Clear();
-            map.MapElements.Add(radius);
-
-            // Update drawing parameters based on slider value
-            // For example, set line thickness or opacity
+            distance = e.NewValue;
+            DrawACircleOnTheMap(e.NewValue, selectedLocation);            
         }
 
         private void BudgetSlider_ValueChanged(object sender, ValueChangedEventArgs e)
@@ -71,25 +66,18 @@ namespace BeanTea
 
         private async void Watch_Button_Clicked(object sender, EventArgs e)
         {
-            var token = await SecureStorage.GetAsync("auth-token");            
-
-            if (!string.IsNullOrEmpty(token)) 
+            var token = await SecureStorage.GetAsync("auth-token");
+            if (!string.IsNullOrEmpty(token))
             {
-                var json = new
-                {
-                    latiude = selectedLocation.Latitude,
-                    longitude = selectedLocation.Longitude,
-                    distance = SearchSlider.Value,
-                    cost = PriceSlider.Value,
-                };
-
-                await _watchservice.AddWatch(JsonConvert.SerializeObject(json));               
-
+                var addWatchRequest = (AddWatchEntity)BindingContext;      
+                addWatchRequest.latitude = selectedLocation.Latitude.ToString();
+                addWatchRequest.longitude = selectedLocation.Longitude.ToString();
+            
+                await _watchservice.AddWatch(JsonConvert.SerializeObject(addWatchRequest)); 
             }
             else
             {
                 await DisplayAlert("Login In", "Please login before notified of a listing in this area.", "cancel");
-
             }
 
         }
@@ -99,5 +87,6 @@ namespace BeanTea
 
         }
 
+      
     }
 }
---
+++ ViewModels/AddWatchEntity.cs
@@ -0,0 +1,37 @@
+﻿
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace BeanTea.ViewModels
+{
+    public partial class AddWatchEntity : ObservableObject
+    {
+        public AddWatchEntity()
+        {
+        }
+
+        [ObservableProperty]
+        bool rentingOption = true;
+
+        [ObservableProperty]
+        bool sharingOption;
+
+        [ObservableProperty]
+        bool sublettingOption;
+
+        [ObservableProperty]
+        public int budget;
+
+        [ObservableProperty]
+        public int distance = 3000;
+
+        public string longitude;
+        public string latitude;
+   
+
+       
+
+    }
+
+}
